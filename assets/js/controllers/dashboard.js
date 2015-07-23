da65f36b105746dbcc0d801d2273febe1223c721
Prashant Raghav <prashantraghav60@gmail.com>
2015-07-23 12:39:32 +0530

    dateTimeFormat service used in controllers

--- assets/js/controllers/dashboard.js
+++ assets/js/controllers/dashboard.js
@@ -1,9 +1,4 @@
-// data from api must be in given format 
-
-/* data = {"stats":{"alerts":15,"totalKms":150,"overall_avg":25,"totalLandingTime":15,"utilization":40},"devices":[{"DeviceID":"1","Vehicle":"UP 13 N 4989","RecivedTime":"2015-06-11 04:07:00","AccOnOff":"On","Location":"Greater Noida","Speed":"9.99","Rpm":"2000","Lat":"28.4962","Lng":"77.536","Temprature":"9.99"},{"DeviceID":"2","Vehicle":" DL 4C 9 2969","RecivedTime":"2015-06-11 04:07:00","AccOnOff":"On","Location":"Mumbai","Speed":"9.99","Rpm":"2000","Lat":"18.975","Lng":"72.8258","Temprature":"9.99"}]} */
-
-
-cth.controller('DashboardController', ['$scope', '$http', '$interval', '$location', function($scope, $http, $interval, $location){
+cth.controller('DashboardController', ['$scope', '$http', '$interval', '$location', 'dateTimeFormat', function($scope, $http, $interval, $location, $dateTimeFormat){
   
   $scope.map = new GMaps({div: '#gmap_marker', lat: AGILE_MAP.initLat, lng: AGILE_MAP.initLng, zoom: AGILE_MAP.initZoom});
   $scope.infoWindow = new google.maps.InfoWindow;
@@ -32,7 +27,7 @@ cth.controller('DashboardController', ['$scope', '$http', '$interval', '$locatio
       return '<div style="color:black">'
              +'<b>Fleet ID </b> '+dev.DeviceID+'<br/>'
              +'<b>Vehicle:</b> '+dev.Vehicle+' ('+dev.Brand+')<br/>'
-             +'<b>RecivedTime:</b> '+dev.RecivedTime+'<br/>'
+             +'<b>RecivedTime:</b> '+$scope.timeFormat(dev.RecivedTime)+' '+$scope.dateFormat(dev.RecivedTime)+'<br/>'
              +'<b>Acc On/Off:</b> '+dev.AccOnOff+'<br/>'
              +'<b>Speed:</b> '+dev.Speed+' <br/>'
              +'<b>Location:</b> '+dev.Location+'<br/>'
@@ -49,6 +44,16 @@ cth.controller('DashboardController', ['$scope', '$http', '$interval', '$locatio
     var myLatlng = new google.maps.LatLng(lat, lng);
     $scope.map.panTo(myLatlng);
   };
+
+
+  $scope.dateFormat = function(dateTime){
+    return $dateTimeFormat.date(dateTime);
+  }
+
+  $scope.timeFormat = function(dateTime){
+    return $dateTimeFormat.time(dateTime);
+  }
+
 
 
   $scope.refresh();
