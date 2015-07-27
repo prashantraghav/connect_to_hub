490f2125fc033c96428c949c25d1fa6ae06e9eb8
Prashant Raghav <prashantraghav60@gmail.com>
2015-07-28 00:45:44 +0530

    history page done with play routes

--- assets/js/controllers/singleDeviceHistory.js
+++ assets/js/controllers/singleDeviceHistory.js
@@ -1,7 +1,7 @@
-cth.controller('SingleDeviceHistoryController', ['$scope', '$http', '$interval', '$location', function($scope, $http, $interval, $location ){
+cth.controller('SingleDeviceHistoryController', ['$scope', '$http', '$interval', '$location', '$timeout', 'dateTimeFormat', 
+                                         function($scope, $http, $interval, $location, $timeout, $dateTimeFormat){
 
-  $scope.trip_id = $location.search()['trip_id']
-  $scope.map = new GMaps({div: '#gmap_marker', lat: AGILE_MAP.initLat, lng: AGILE_MAP.initLng, zoom: AGILE_MAP.initZoom});
+  $scope.map = new GMaps({div: '#gmap_marker', lat: AGILE_MAP.initLat, lng: AGILE_MAP.initLng, zoom: 10});
   $scope.path = [];
 
   $scope.mapLivePolyline = function(){
@@ -15,28 +15,41 @@ cth.controller('SingleDeviceHistoryController', ['$scope', '$http', '$interval',
 
   $scope.tripLocation = function(){
     return { 
-      source: {lat: $scope.path[0][0], lng: $scope.path[0][1]},
-      destination: {lat: $scope.path[$scope.path.length-1][0], lng: $scope.path[$scope.path.length-1][1]}
+      source: {lat: $scope.routes[0]['lat'], lng: $scope.routes[0]['lng']},
+      destination: {lat: $scope.routes[$scope.routes.length-1]['lat'], lng: $scope.routes[$scope.routes.length-1]['lng']}
     };
   }
 
-  $scope.mapFocus = function(){
-    var myLatlng = new google.maps.LatLng($scope.tripLocation().destination.lat, $scope.tripLocation().destination.lng);
+  $scope.mapFocus = function(lat, lng){
+    var myLatlng = new google.maps.LatLng(lat, lng);
     $scope.map.panTo(myLatlng);
   };
 
-  
+
   $scope.showMarkers = function(){
     var _location = $scope.tripLocation();
     $scope.map.addMarker({lat: _location.destination.lat, lng: _location.destination.lng, icon: AGILE_MAP.marker.icon});
     $scope.map.addMarker({lat: _location.source.lat, lng: _location.source.lng, icon: AGILE_MAP.marker.icon});
   }
 
-  $scope.drawMap = function(){
+  $scope.drawMap = function(play){
+    arguments.callee.i = (play) ? 0 : ++arguments.callee.i;
+    lat = $scope.routes[arguments.callee.i].lat;
+    lng = $scope.routes[arguments.callee.i].lng;
+    $scope.path.push([lat, lng]);
     $scope.mapLivePolyline();
-    $scope.showMarkers();
-    $scope.mapFocus();
-    $scope.map.setZoom(10);
+    $scope.mapFocus(lat, lng);
+    if(arguments.callee.i < $scope.routes.length-1){
+      $timeout(function(){
+        $scope.drawMap()
+      }, 2000);
+    }
+  }
+
+  $scope.playRoute = function(){
+    $scope.path = [];
+    $scope.map.removePolylines();
+    $scope.drawMap(true);
   }
 
   $scope.makeChart = function(){
@@ -44,20 +57,34 @@ cth.controller('SingleDeviceHistoryController', ['$scope', '$http', '$interval',
   }
 
   $scope.refresh = function() {
-    url = SINGLE_DEVICE_HISTORY_DATA_URL+'?trip='+$scope.trip_id;
+    trip_id = $location.search()['trip_id']
+    url = SINGLE_DEVICE_HISTORY_DATA_URL+'?trip='+trip_id;
     $http.get(url).success(function(data) {
+
       $scope.trip = data.trip;
+      $scope.tripHistory = data.tripHistory;
       $scope.routes = data.routes;
-      $scope.chart = data.chart
-      $.each($scope.routes, function(i, route){
-        $scope.path.push([route.lat, route.lng]);
-      });
-      $scope.drawMap();
+      $scope.chart = data.chart;
+
+      $scope.playRoute();
+      $scope.showMarkers();
       $scope.makeChart();
     });
   };
 
+  $scope.goToHistoryPage = function(trip_id){
+    $location.path('singleDeviceHistory').search({'trip_id': trip_id});
+    $scope.refresh();
+  }
+
+  $scope.dateFormat = function(dateTime){
+    return $dateTimeFormat.date(dateTime);
+  }
+
+  $scope.timeFormat = function(dateTime){
+    return $dateTimeFormat.time(dateTime);
+  }
+
   $scope.refresh();
 
-  //$interval( $scope.refresh, 2000); 
 }]);
